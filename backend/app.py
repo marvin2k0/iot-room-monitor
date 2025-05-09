64957952b8ef3ea7671ff46a1dbef7692b8a9b0f
Marvin <marvin@leiers.org>
2025-05-09 21:35:54 +0200

    Add backend

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:4200"])
+
+@app.route("/", methods=["POST"])
+def receive_sensor_data():
+    data = request.get_json()
+    
+    temperature = data.get("temperature")
+    humidity = data.get("humidity")
+
+    if temperature == None or humidity == None:
+        return jsonify({"error": "Incomplete request body"}), 400
+    
+    return jsonify({"message": "Received data"}), 200
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080)
